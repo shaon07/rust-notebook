176d976f17ade716da547f7d78fb8cf9c920e21b
shaon07 <alishaon078@gmail.com>
2026-02-08 23:44:28 +0600

    ReactAngle and simplify area calculations with methods

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,39 @@
-#[derive(Debug)]
-struct Reactangle {
+struct ReactAngle {
     width: u32,
     height: u32,
 }
+
+impl ReactAngle {
+    // in methods first param will be by default self.
+    fn area(&self) -> u32 {
+        return self.width * self.height;
+    }
+
+    // now lets take others params
+    fn area_with_unit(&self, unit: &str) -> u32 {
+        let area: u32 = self.height * self.height;
+        println!("The Area of the reactangle is {}{}", area, unit);
+        return area;
+    }
+}
+
+struct Square {
+    width: u32,
+    height: u32,
+}
+
+impl Square {
+    // Associated Functions. its use to do something with the struct itself. and return Self type
+    fn create(size: u32) -> Self {
+        Self { width: size, height: size }
+    }
+}
+
 fn main() {
-    // Lets understand why we need to use struct. to solve a problem
+    let rect_1: ReactAngle = ReactAngle { width: 40, height: 60 };
+    println!("The area of the reactangle is {}", rect_1.area());
+    rect_1.area_with_unit("KM");
 
-    // 1. First we will solve a problem without using struct
-
-    // Problem: Find the Area of a Reactangle
-    let width: u32 = 100;
-    let height: u32 = 150;
-    // here is the problem if the problem is a huge calucaltion and may take more params. then we might pass a wrong param at wrong position
-    let result: u32 = reactangle_area_without_struct(width, height);
-    println!("Area of the Reactangle is: {}", result);
-
-    // so lets try to solve it with a tuple
-    let rect: (u32, u32) = (100, 200);
-    // here is the problem is we dont which position value is width and which one is height. lets say we mistakenly pass width param value as height so on.
-    let result: u32 = reactangle_area_without_struct(rect.0, rect.1);
-    println!("Area of the Reactangle is: {}", result);
-
-    // so lets try to solve it with struct.
-    let reactangle: Reactangle = Reactangle {
-        height: 144,
-        width: 135,
-    };
-    let result: u32 = reactangle_area_struct(reactangle);
-    println!("Area of the Reactangle is: {}", result);
-
-    // so lets look at the struct way to solve the problem. its so much clean and understandable.
-    // we can also solve this using only taking the reference of the reactangle.
-
-    // so now solve it with taking reference instead of taking the full ownership.
-
-    let rect: Reactangle = Reactangle { width: 260, height: 300 };
-    let res: u32 = reactangle_area_struct_with_borrowing(&rect);
-    println!("The width:{}, height:{} and area: {}", rect.width, rect.height, res);
-}
-
-fn reactangle_area_without_struct(width: u32, height: u32) -> u32 {
-    let area: u32 = width * height;
-    return area;
-}
-
-fn reactangle_area_struct(reactangle: Reactangle) -> u32 {
-    let area: u32 = reactangle.width * reactangle.height;
-    return area;
-}
-
-fn reactangle_area_struct_with_borrowing(reactangle: &Reactangle) -> u32 {
-    let area: u32 = reactangle.width * reactangle.height;
-    return area;
+    let sq: Square = Square::create(10);
+    println!("The Dimension of the Square is width:{} and height:{}", sq.width, sq.height);
 }
